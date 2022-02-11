3dfce425fb9c8eaefa1b3af42091bf8accd60199
Antón Vega <anton.paidoslalin@gmail.com>
2022-02-11 16:13:06 +0100

    Webpack

---
+++ 03-webpack-inicial/dist/main.js
@@ -0,0 +1,1 @@
+(()=>{"use strict";(e=>{console.log("Creando etiqueta h1");const n=document.createElement("h1");n.innerText="Hola, Antón",document.body.append(n)})()})();
--- 03-webpack-inicial/src/index.js
+++ 03-webpack-inicial/src/index.js
@@ -1,12 +1,4 @@
+import {saludar} from './js/componentes.js';
+const nombre = 'Antón';
 
-
-const saludar = (nombre) => {
-    console.log('Creando etiqueta h1');
-
-    const h1 = document.createElement('h1');
-    h1.innerText=`Hola, ${nombre}`;
-    document.body.append (h1);
-
-}
-
-saludar ('Antón');
+saludar (nombre);
