1d198b48c5a53dd8dd11dde1c05e8b7e953cd4ba
Antón Vega <anton.paidoslalin@gmail.com>
2022-02-15 10:46:59 +0100

    abc

--- 03-webpack-inicial/dist/main.js
+++ 03-webpack-inicial/dist/main.js
@@ -26,7 +26,7 @@ eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _js_
   \*******************************/
 /***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"saludar\": () => (/* binding */ saludar)\n/* harmony export */ });\nconst saludar = (nombre) => {\n    console.log('Creando etiqueta h1');\n\n    const h1 = document.createElement('h1');\n    h1.innerText=`Hola, ${nombre}`;\n    document.body.append (h1);\n\n}\n\n//# sourceURL=webpack://webpack-inicial/./src/js/componentes.js?");
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"saludar\": () => (/* binding */ saludar)\n/* harmony export */ });\nconst saludar = (nombre) => {\n    console.log('Creando etiqueta h1');\n\n    const h1 = document.createElement('h1');\n    h1.innerText=`Hola, ${nombre}!!!`;\n    document.body.append (h1);\n\n}\n\n//# sourceURL=webpack://webpack-inicial/./src/js/componentes.js?");
 
 /***/ })
 
---
+++ 03-webpack-inicial/webpack.config.js
@@ -0,0 +1,26 @@
+const HtmlWebpack = require ('html-webpack-plugin');
+
+module.exports={
+    mode:'development',
+    output:{
+        clean:true
+    },
+    module : {
+        rules:[{
+                test:/\.html$/,
+                loader: 'html-loader',
+                options:{
+                    sources: false
+                }
+            },
+            {
+                test:/\.css$/,
+                use:['style-loader', 'css-loader']
+            } 
+        ]
+    },
+    optimization:{},
+    plugins: [
+         new HtmlWebpack()
+    ]
+}
